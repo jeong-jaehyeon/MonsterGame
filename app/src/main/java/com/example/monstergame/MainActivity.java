54a83f789ed1c587b1a9d118eca5c91b4c78c729
Jeong Jae Hyeon <wolgus104@naver.com>
2020-12-08 01:24:22 +0900

    2번째 작업
    wait, replay 이미지 생성.
    랜덤 정수 생성하여 볼에 확률 부여.

--- app/src/main/java/com/example/monstergame/MainActivity.java
+++ app/src/main/java/com/example/monstergame/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
 
     ImageView monsterAnimationImage;
     AnimationDrawable animationDrawable;
+    ImageView waitImage;
+    ImageView replayButton;
 
 //    ImageView firstBall;
 //    ImageView secondBall;
@@ -28,14 +30,68 @@ public class MainActivity extends AppCompatActivity {
         monsterAnimationImage = findViewById(R.id.monster_animation_image);
         animationDrawable = (AnimationDrawable) monsterAnimationImage.getDrawable();
         animationDrawable.start();
+        waitImage = findViewById(R.id.wait_image);
+        replayButton = findViewById(R.id.replayButton);
+    }
+
+    public int setBall() {
+        animationDrawable.stop();
+        monsterAnimationImage.setVisibility(View.GONE);
+        waitImage.setVisibility(View.VISIBLE);
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        double num = Math.random();
+        int ranNum = (int) (num * 100);
+        return ranNum;
     }
 
     public void ball_click(View view) {
         switch (view.getId()) {
-            case R.id.first_Ball :
+            case R.id.replayButton:
+                waitImage.setVisibility(View.GONE);
+                monsterAnimationImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+                break;
+
+            case R.id.first_Ball:
+                if (setBall() <= 32) {
+                    //[?]이 당첨되어 [33]% 의 확률로 포획성공 [num] ,
+                    System.out.println("성공");
+                } else {
+                    System.out.println("실패");
+                    //포획실패
+                }
+                break;
+
+            case R.id.second_Ball:
+                if (setBall() <= 65) {
+                    //포획성공
+                    System.out.println("성공");
+                } else {
+                    System.out.println("실패");
+                    //포획실패
+                }
+                break;
+
+            case R.id.third_Ball:
+                if (setBall() <= 99) {
+                    //포획성공
+                    System.out.println("성공");
+                } else {
+                    System.out.println("실패");
+
+                    //포획실패
+                }
+                break;
 
         }
 
     }
+
 
 }
